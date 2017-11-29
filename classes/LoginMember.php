56bb3ad63707edbfafc6818cd4178eca686c6910
Matthew Thompson <thompson7725@saskpolytech.ca>
2017-11-29 08:56:10 -0600

    Created a loginmember class and coded part of the memberlogin php page

---
+++ classes/LoginMember.php
@@ -0,0 +1,24 @@
+<?php
+use DB3\Model;
+
+/**
+ * Is basically a dummy "Member" used for logging in, since having the alias OR email address to log in on would create validation issues
+ *
+ * @version 1.0
+ * @author cst243
+ */
+class LoginMember extends Model
+{
+    public $aliasEmail; // holds either the alias, or the email depending on what the user tried to sign in with.
+    public $password;
+
+    public function __construct()
+    {
+        // Note, this will NEVER be stored in the DB, so don't define columns
+        // Do define labels though
+        $this->setLabel('aliasEmail', 'Alias / Email');
+        $this->setLabel('password', 'Password');
+    }
+
+    // NOTE ADD VALIDATION HERE
+}
--- cst243cst209cst220cweb280a5/pages/memberlogin.php
+++ cst243cst209cst220cweb280a5/pages/memberlogin.php
@@ -1,4 +1,6 @@
 <?php
+// Make sure to start the session!
+session_start();
 
 use DB3\DB3; // instead of include or require we  can use the keyword 'use' to include classes in a namespace.
 use DB3\Filter;
@@ -9,11 +11,41 @@ spl_autoload_register(function ($class) {
     require_once '..\\classes\\' .$class . '.php';
 });
 
+// Holds whether or not the user posted the form
 $isPosted = $_SERVER['REQUEST_METHOD'] === 'POST';
 
+// Stores if the user is authenticated
 $isAuthenticated = false;
 
-$member = new Member()
+// Create a new member login
+$member = new LoginMember();
+// If the form is posted, store the information from the form 
+if ($isPosted)
+{
+	$member->aliasEmail = $_POST['alias'];
+    $member->password = $_POST['password'];
+}
+
+// Try to validate the entered data
+$isValidPost = $isPosted && $member->validate();
+// If it is a valid submission, open the database and check to see if a member exists that matches
+if ($isValidPost)
+{
+	// open db
+    $db = new DB3('../db/imageranker.db');
+    //see if the member exists based off of the email OR alias
+    $filters = array(new Filter('email', $member->aliasEmail), new Filter('alias', $member->aliasEmail));
+    $logins = $db->selectSome(new Member(), $filters, false); // Use the OR operator
+    // Close the database
+    $db->close();
+    $db = null;
+}
+
+
+// Create the form inputs
+// Email / Alias
+
+
 
 
 ?>
