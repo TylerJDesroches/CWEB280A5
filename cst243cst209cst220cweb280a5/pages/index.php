c35967f9fa5f536fdd52c9e465f7853abc223ed9
Matthew Thompson <thompson7725@saskpolytech.ca>
2017-12-06 11:17:58 -0600

    .

--- cst243cst209cst220cweb280a5/pages/index.php
+++ cst243cst209cst220cweb280a5/pages/index.php
@@ -1,5 +1,41 @@
 <?php
+use DB3\DB3;
+use DB3\Filter;
+
 session_start();
+
+spl_autoload_register(function ($class) {
+    require_once '..\\..\\classes\\' .$class . '.php';
+});
+
+// Open the database to query for the images
+$db = new DB3('../../db/imageranker.db');
+// Get the images ordered by views... Would love to use sql to only select the top 5, but our DB3 isn't designed for this.
+$orders = array('views'=>'DESC');
+$filters = array(new Filter('approved', true)); // only images that are approved
+$topImages = $db->selectSomeOrder(new Image(), $orders, $filters);
+
+// Reduce the top images to just 5
+$topImages = array_slice($topImages, 0, 4);
+
+// Get all the images ordered by date
+$orders = array('id'=>'DESC');
+$allImages = $db->selectSomeOrder(new Image(), $orders, $filters);
+
+// Get all the members out of the database
+$allMembers = $db->selectAll(new Member());
+
+// Close the database, real quick like sanic
+$db->close();
+$db = null;
+
+// Create an array where the key is the id of the member
+$keys = array();
+foreach ($allMembers as $member)
+{
+	$keys[] = $member->memberId;
+}
+$allMembers = array_combine($keys, $allMembers);
 
 ?>
 
@@ -7,76 +43,39 @@ session_start();
 <html>
 <head>
     <title>Image Ranker</title>
-    <!--Include knockout and jquery-->
+    <!--Include jquery-->
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min.js"></script>
-
-    <script>
-
-        var viewModel = {
-            topImages: ko.observableArray(),
-            allImages: ko.observableArray(),
-            getImages: function ()
-            {
-                // make the json/ajax call
-                $.getJSON('../json/getimages.php', function (jsonImages)
-                {
-                    // Success function
-                    observableAllImages = Array();
-                    for (var i = 0; i < jsonImages['allImages'].length; i++) {
-                        observableAllImages.push(new observableImage(jsonImages['allImages'][i]));
-                    }
-                    viewModel.allImages(observableAllImages);
-
-                    observableTopImages = Array();
-                    for (var i = 0; i < jsonImages['topImages'].length; i++) {
-                        observableTopImages.push(new observableImage(jsonImages['topImages'][i]));
-                    }
-                    viewModel.topImages(observableTopImages);
-
-                });
-            }
-        };
-
-        // On document load
-        $(function ()
-        {
-            // Get the images
-            viewModel.getImages();
-            // Bind the view model object to the DOM
-            ko.applyBindings(viewModel);
-        });
-
-        // An image object
-        function observableImage(jsonObj)
-        {
-            this.id = ko.observable(jsonObj.id);
-            this.path = ko.observable(jsonObj.path);
-            this.memId = ko.observable(jsonObj.memId);
-            this.caption = ko.observable(jsonObj.caption);
-            this.views = ko.observable(jsonObj.views);
-            this.approved = ko.observable(jsonObj.approved);
-            this.link = 'details.php?id=' + this.id();
-
-        }
-
-    </script>
 
 </head>
 <body>
     <h1>Trending</h1>
-    <ul data-bind="foreach: topImages">
-        <li><a data-bind="attr:{href: link}"><img data-bind="attr:{src: path}" /></a></li>
+    <ul>
+    <?php
+    // Loop through all the top images and put them on the page
+    foreach ($topImages as $currentImage)
+    { ?>
+    	<li><a href="details.php?id=<?= $currentImage->id ?>"><img src="<?= $currentImage->path ?>" /></a></li>
+    <?php
+    } ?>
     </ul>
 
     <h1>All Images</h1>
-    <ul data-bind="foreach: allImages">
-        <li>
-            <div><a data-bind="attr:{href: link}"><img data-bind="attr:{src: path}" /></a></div>
-            <div data-bind="text: caption"></div>
-            <div data-bind=""></div>
-
+    <ul>
+    <?php
+    // Loop through all the normal images and output their information
+    foreach ($allImages as $currentImage)
+    { 
+        // Get the current member associated with the posted image
+        $currentMember = $allMembers[$currentImage->memId];
+        ?>
+    	<li>
+            <div><a href="details.php?id=<?= $currentImage->id ?>"><img src="<?= $currentImage->path ?>" /></a></div>
+            <div>Caption: <?= $currentImage->caption ?></div>
+            <div>Alias: <?= $currentMember->alias ?></div>
+            <div><img href="<?= $currentMember->profileImgPath ?>" /></div>
         </li>
+    <?php
+    } ?>
     </ul>
 </body>
 </html>
